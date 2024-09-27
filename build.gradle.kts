99c65b36cbb6b25584288044ca5f24f25526be5d
*Garduque <rivandreiofficial@gmail.com>
2024-09-27 13:02:43 +0800

    sana gumana ka pleaAAAAAAAAAAAAAAAAAAAAAAAse

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    extra.apply {
+        set("compose_compiler_version", "1.5.3")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
